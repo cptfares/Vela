92b7bea323fba800bcca3f9790330b82e576f434
fares anes <58134689+cptfares@users.noreply.github.com>
2025-03-13 23:29:50 +0100

    Update TestimonialData.js
    adding more testminals

--- components/Testimonial/TestimonialData.js
+++ components/Testimonial/TestimonialData.js
@@ -20,12 +20,33 @@ const slides = [
     star: 5,
   },
   {
-    key: 1,
-    Image: "/images/mondher1.png",
-    Name: "Mondher Gorgi",
-    position: "CEO of Trio",
+    key: 2,
+    Image: "/images/testm1.png",
+    Name: "Triki Abderrahmen ",
+    position: "CEO of vistara",
     Descriptions:
-      "Vela is a great team to work with. They're knowledgeable, professional, and always go the extra mile.",
+      "Vela did an outstanding job with our website! His creativity and attention to detail were impressive, and he really captured the essence of Vistara. The site is sleek, easy to navigate, and SEO-optimized, which has already helped us reach more clients. We highly recommend Vela for anyone looking to elevate their online presence!",
+    class: "AboveContainer",
+    star: 5,
+  },
+    {
+    key: 3,
+    Image: "/images/testm2.png",
+    Name: "Oussema zouari  ",
+    position: "CEO of East west",
+    Descriptions:
+      "Working with Vela was a great experience! He created an amazing marketing video for our glass and door repair company that truly captured our services. Professional, creative, and efficient—highly recommend Vela for top-quality video production!",
+    class: "AboveContainer",
+    star: 5,
+  },
+  ,
+    {
+    key: 4,
+    Image: "/images/testm3.png",
+    Name: "Paul Chapman",
+    position: " CEO of Glass Genius",
+    Descriptions:
+      "Working with Vela was an absolute pleasure! He designed a fantastic website for Glass Genius that truly reflects our brand. Vela’s creativity, attention to detail, and professionalism shone through in every aspect of the site. It’s user-friendly, visually appealing, and perfectly showcases our services. The whole process was smooth, and Vela delivered exactly what we envisioned. Highly recommend his services for anyone looking for a top-notch website!",
     class: "AboveContainer",
     star: 5,
   },
